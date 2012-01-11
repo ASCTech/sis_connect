023f327b18d5ea311ae1cce6416ad737f1cc6f91
John Colvin <colvin.68@osu.edu>
2012-01-11 11:22:03 -0500

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SisConnect::Application.config.secret_token = '3785d4458f23eb91538c2d7c98c233b1eed204f84548b395515ee6844af19a093c8d05670335e9fe191d1d0e81e11668d29cda2e57544c6e833bddb393be4561'
