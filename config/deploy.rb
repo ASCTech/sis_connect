2cdbb526710f131d9b79abc191a1a0d137b43622
mikegee <gee.24@osu.edu>
2012-05-04 23:10:08 -0400

    got deployed to staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,11 +6,12 @@ set :keep_releases, 11
 
 set :use_sudo, false
 set :application, "sis_connect"
+set :sub_uri, ''
 
 set :deploy_to, "/var/www/apps/#{application}"
 
 set :scm, :git
-set :repository,  "git@github.com:ASCTech/sis_connect.git"
+set :repository, "git@github.com:ASCTech/sis_connect.git"
 set :branch, 'master'
 set :deploy_via, :remote_cache
 
@@ -22,6 +23,22 @@ task :staging do
   role :web, "ruby-test.asc.ohio-state.edu"
   role :db,  "ruby-test.asc.ohio-state.edu", :primary => true
   set :rails_env, 'staging'
+  set :sub_uri, application
+end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+
+    # hit the server once, so the first user doesn't get a slow page load
+    run "curl --insecure --silent https://localhost/#{sub_uri} > /dev/null"
+  end
+
+  task :seed, :roles => :app do
+    run "cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} db:seed"
+  end
 end
 
 before "deploy:assets:precompile" do
