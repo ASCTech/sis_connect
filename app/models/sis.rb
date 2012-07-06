2e28d22fc37ba531343f45944d7871d0c0048215
mikegee <gee.24@osu.edu>
2012-07-06 11:20:22 -0400

    moved db connection string junk from conf to model

--- app/models/sis.rb
+++ app/models/sis.rb
@@ -1,5 +1,10 @@
 class SIS < ActiveRecord::Base
 
-  establish_connection configurations['sis']
+  db_settings = {
+    :adapter  => 'oracle_enhanced',
+    :database => 'dwhcrptdb.erp.ohio-state.edu:1772/DWHCRPT.erp.ohio-state.edu',
+    :encoding => 'utf8'
+  }
+  establish_connection db_settings.merge(configurations['sis_credentials'])
 
 end
